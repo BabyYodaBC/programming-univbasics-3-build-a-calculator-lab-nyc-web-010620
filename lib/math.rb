6e81542b16d407b6b17578a982e60c1b54bbd7b5
ricky wahba <rjwahba@gmail.com>
2019-12-25 15:15:30 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,21 +4,25 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  s
+  diff = num2 - num1
+  return diff
 end
 
 def division(num1, num2)
-
+  quo = num2 / num1
+  return quo
 end
 
 def multiplication(num1, num2)
-
+  prod = num2 * num1
+  return prod
 end
 
 def modulo(num1, num2)
-
+  rem = num2 % num1
+  return rem
 end
 
 def square_root(num)
-
+  
 end
